9c2926196a8399320891df9cc9be411222a4014e
DwarapuNikhil <69182502+DwarapuNikhil@users.noreply.github.com>
2020-08-04 15:29:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 function validate() {
 	
-	window.alert("Edit Successful");
+	var n=document.getElementById("name").value;
+	var p=document.getElementById("price").value;
+	var date=document.getElementById("datetime").value;
+	var fd=document.getElementById("fd").checked;
+	var ys=document.getElementById("Yes").checked;
+	var no=document.getElementById("No").checked;
+	if(n.length===0 || p.length===0 || date.length===0||(!fd)|| (ys===0 && no===0)){
+		
+		window.alert("Required Fields are Empty");
+	}
+	else{
+		window.alert("Edit Successful");
+	}
 	return false;
 }
